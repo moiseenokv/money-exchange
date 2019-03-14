c7d204cf1f9d1ce8611cbeed5b7b4a722e40364c
VitalMos <moiseenokv@gmail.com>
2019-03-14 12:05:31 +0300

    add exchange function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var money=parseInt(currency);
+    var change={ "H": 50, "Q": 25, "D": 10, "N":5, "P":1 };
+    var obj={};
+    for (var key in change) {
+        if(money<=0){
+            return obj;
+            break;
+        }else if(money >=10001){
+            return obj={error: "You are rich, my friend! We don't have so much coins for exchange"};
+            break; 
+        }else{
+            check=parseInt(money/change[key]);
+            check_ost=money%change[key];
+            if(check>0){
+                obj[key]=check;
+                if(check_ost==0){
+                    return obj;
+                    break; 
+                }else{
+                    money = money-(money-check_ost);
+                }
+            } 
+        }
+    }
 }
